80c5ec47314ada3e51469ed4779763ff0bdfa1f0
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-04-24 17:25:31 +0100

    created AuthController and RegisterRequest to validate creating new user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    //
+    public function register (RegisterRequest $request): string
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('first_name'),
+            'email' => $request->input('first_name'),
+            'password' => Hash::make($request->input('first_name')),
+        ]);
+        return response($user, Response::HTTP_CREATED);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello', fn() =>'hello');
+Route::post('register',[AuthController::class, 'register']);
