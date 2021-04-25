9a67c0e6ce49b6207ae2bdf9161cb1500e818cb2
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-04-25 06:52:33 +0100

    created user controller and populated functions

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        return User::paginate(1);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create(
+            $request->only('first_name', 'last_name', 'email',) + ['password' => \Hash::make(1234)]
+        );
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->only('first_name', 'last_name', 'email'));
+
+        return \response($user, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return \response(null, Response::HTTP_NO_CONTENT);
+    }
+}
