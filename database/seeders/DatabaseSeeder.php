1bb79223766fb788e499f271fe511b9382eefbd0
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-04-26 06:43:18 +0100

    seperated seeders in to their own folders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,39 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Role::factory()->create([
-            'name' => 'Admin'
+        $this->call([
+           PermissionSeeder::class,
+           RoleSeeder::class,
+           UserSeeder::class
         ]);
 
-        Role::factory()->create([
-            'name' => 'Editor'
-        ]);
 
-        Role::factory()->create([
-            'name' => 'Viewer'
-        ]);
-
-         User::factory(100)->create();
-
-         User::factory()->create([
-             'first_name' => 'Admin',
-             'last_name' => 'Admin',
-             'email' => 'admin@admin.com',
-             'role_id' => 1
-         ]);
-
-        User::factory()->create([
-            'first_name' => 'Editor',
-            'last_name' => 'Editor',
-            'email' => 'editor@editor.com',
-            'role_id' => 2
-        ]);
-
-        User::factory()->create([
-            'first_name' => 'Viewer',
-            'last_name' => 'Viewer',
-            'email' => 'viewer@viewer.com',
-            'role_id' => 3
-        ]);
     }
 }
